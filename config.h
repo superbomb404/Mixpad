35aff43ce8f8170aeeaf9e4c0c09caa41db69387
superbomb404 <67756856+superbomb404@users.noreply.github.com>
2024-09-29 21:33:54 +0800

    适配v1.1 PCB 代码优化

--- config.h
+++ config.h
@@ -19,10 +19,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MK_3_SPEED
 #define TERMINAL_HELP
-#define I2C_SDA_PIN GP2  // 将 SDA 引脚设置为 GP1
-#define I2C_SCL_PIN GP3  // 将 SCL 引脚设置为 GP0
+
 #define I2C_DRIVER I2CD1
+#define I2C_SDA_PIN GP2  // 将 SDA 引脚设置为 GP2
+#define I2C_SCL_PIN GP3  // 将 SCL 引脚设置为 GP3
+
 // #define I2C1_SCL_PAL_MODE 3
 // #define I2C1_SDA_PAL_MODE 3
-// #define OLED_DISPLAY_ADDRESS 0x3C
-#define OLED_DISPLAY_128X64
+// #define OLED_DISPLAY_ADDRESS 0x3C   //屏幕地址
+// #define DISABLE_UART
+
+#define OLED_DISPLAY_128X64  //设置屏幕分辨率
+#define OLED_BRIGHTNESS 255  //设置屏幕亮度
+#define OLED_SCROLL_TIMEOUT 300000  //在 OLED 静止 300000 毫秒(300秒)后滚动 OLED 屏幕 设置为 0 则禁用
+#define OLED_TIMEOUT 600000   //在 OLED 更新 600000 毫秒(600秒)后关闭 OLED 屏幕 设置为 0 则禁用
+#define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2   //设置i2c通信为快速模式(提高屏幕刷新率)
+#define I2C1_CLOCK_SPEED 400000  //设置i2c通信速率(提高屏幕刷新率)
