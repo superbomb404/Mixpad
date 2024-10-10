e9635dd15680a6b0f25b9e9348ac3ee631fb0a0d
superbomb404 <67756856+superbomb404@users.noreply.github.com>
2024-10-10 23:02:27 +0800

    代码精简

--- config.h
+++ config.h
@@ -1,19 +1,3 @@
-/*
-Copyright 2020 MisonoWorks
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #pragma once
 
@@ -24,35 +8,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define I2C_SDA_PIN GP2  // 将 SDA 引脚设置为 GP2
 #define I2C_SCL_PIN GP3  // 将 SCL 引脚设置为 GP3
 
-// #define I2C1_SCL_PAL_MODE 3
-// #define I2C1_SDA_PAL_MODE 3
-// #define OLED_DISPLAY_ADDRESS 0x3C   //屏幕地址
-// #define DISABLE_UART
-
-
 #define OLED_DISPLAY_128X64  //设置屏幕分辨率
 #define OLED_BRIGHTNESS 255  //设置屏幕亮度
 // #define OLED_SCROLL_TIMEOUT 300000          //在 OLED 静止 300000 毫秒(300秒)后滚动 OLED 屏幕 设置为 0 则禁用
 #define OLED_TIMEOUT 120000                    //在 OLED 更新 120000 毫秒(120秒)后关闭 OLED 屏幕 设置为 0 则禁用
-// #define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2   //设置i2c通信为快速模式(提高屏幕刷新率)
-// #define I2C1_CLOCK_SPEED 400000             //设置i2c通信速率(提高屏幕刷新率)
-
-//自定义屏幕分辨率
-// #define OLED_DISPLAY_WIDTH 128
-// #define OLED_DISPLAY_HEIGHT 64
+#define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2   //设置i2c通信为快速模式(提高屏幕刷新率)
+#define I2C1_CLOCK_SPEED 400000             //设置i2c通信速率(提高屏幕刷新率)
 
 
-// //RGB阵列
-// #define WS2812_DI_PIN GP16
-// // #define WS2812_LED_COUNT 22
-// #define RGB_MATRIX_LED_COUNT 22
-// #define RGBLIGHT_LED_COUNT 22
-// #define ENABLE_RGB_MATRIX_CYCLE_ALL
 
-
-/* Required for SH1106 Oled Driver */
+/* SH1106屏幕驱动 */
 #define OLED_IC OLED_IC_SH1106
-#define OLED_COLUMN_OFFSET 2 // SH1106 screen is a little off to the left
+#define OLED_COLUMN_OFFSET 2  //SH1106屏幕偏移
 
 #define I2C1_TIMINGR_PRESC 0x00U
 #define I2C1_TIMINGR_SCLDEL 0x03U
--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -1,19 +1,3 @@
-/*
-Copyright 2020 MisonoWorks
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #include QMK_KEYBOARD_H
 
@@ -41,35 +25,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-/*
-//RGB
-led_config_t g_led_config = { {
-  // Key Matrix to LED Index
-  { NO_LED, 0, 1, 2, 3 },
-  { NO_LED, 4, 5, 6, 7 },
-  { 8, 9, 10, 11, 12 },
-  { 13, 14, 15, 16, NO_LED },
-  { 17, 18, 19, 20, 21 }
-}, {
-  // LED Index to Physical Position
-  			 { 1,  4 }, { 2,  4 }, { 3,  4 }, { 4,  4 },
-  			 { 1,  3 }, { 2,  3 }, { 3,  3 }, { 4,  3 },
-  { 0,  2 }, { 1,  2 }, { 2,  2 }, { 3,  2 }, { 4,  2 }, 
-  { 0,  1 }, { 1,  1 }, { 2,  1 }, { 3,  1 },
-  { 0,  0 }, { 1,  0 }, { 2,  0 }, { 3,  0 }, { 4,  0 }
-}, {
-  // LED Index to Flag
-     4, 4, 4, 4,
-     4, 4, 4, 4,
-  4, 4, 4, 4, 4,
-  4, 4, 4, 4,
-  4, 4, 4, 4, 4
-} };
-*/
-
-
-
-
+//编码器配置
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [DEFAULT] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_F13, KC_F14), ENCODER_CCW_CW(KC_F15, KC_F16), ENCODER_CCW_CW(KC_F17, KC_F18), ENCODER_CCW_CW(KC_F19, KC_F20), ENCODER_CCW_CW(KC_F21, KC_F22), ENCODER_CCW_CW(KC_UP, KC_DOWN)  },
+    //                                 Encoder 1                              Encoder 2                         Encoder 3                         Encoder 4                     Encoder 5                      Encoder 6                      Encoder 7
+    [FN] =        { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)  }
+    //                    Encoder 1                          Encoder 2
+};
+#endif
 
 
 
@@ -77,20 +41,6 @@ led_config_t g_led_config = { {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_90;
 }
-
-
-//显示测试文字
-/*
-bool oled_task_user(void) {
-    oled_write_P(PSTR("Test1"),false);
-    oled_set_cursor(10,1);
-    oled_write_P(PSTR("Test2\n"),false);
-    oled_write_P(PSTR("Test3"),false);
-    return false;
-}
-*/
-
-
 
 
 //显示图片
@@ -138,12 +88,3 @@ bool oled_task_user(void) {
 #endif
 
 
-//编码器配置
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [DEFAULT] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_F13, KC_F14), ENCODER_CCW_CW(KC_F15, KC_F16), ENCODER_CCW_CW(KC_F17, KC_F18), ENCODER_CCW_CW(KC_F19, KC_F20), ENCODER_CCW_CW(KC_F21, KC_F22), ENCODER_CCW_CW(KC_UP, KC_DOWN)  },
-    //                                 Encoder 1                              Encoder 2                         Encoder 3                         Encoder 4                     Encoder 5                      Encoder 6                      Encoder 7
-    [FN] =        { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)  }
-    //                    Encoder 1                          Encoder 2
-};
-#endif
