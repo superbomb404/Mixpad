e11366d63bfaff2648ef15b5b444644bb149a7e4
superbomb404 <67756856+superbomb404@users.noreply.github.com>
2024-10-11 00:04:22 +0800

    添加第三键层

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -3,7 +3,8 @@
 
 enum layers {
     DEFAULT,
-    FN
+    FN,
+    FN1
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -12,16 +13,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_MPRV, KC_P7,  KC_P8,   KC_P9, //row0
                     KC_MNXT, KC_P4,  KC_P5,   KC_P6, //row1
         TO(FN),     KC_MPLY, KC_P1,  KC_P2,   KC_P3, //row2
-        KC_F13,     KC_F14,  KC_P0,  KC_PDOT,        //row3
+        KC_F13,     TO(FN1), KC_P0,  KC_PDOT,        //row3
         KC_F15,     KC_F16,  KC_F17, KC_F18,  KC_F19 //row4
     ),
 
     [FN] = LAYOUT(
-                     RGB_SPD, RGB_SPI, RGB_RMOD, RGB_MOD,
-                     EE_CLR,  RGB_TOG, XXXXXXX,  XXXXXXX,
-        TO(DEFAULT), QK_BOOT, XXXXXXX, XXXXXXX,  XXXXXXX,
-        KC_F13,      KC_F14,  XXXXXXX, XXXXXXX, 
-        KC_F15,      KC_F16,  KC_F17,  KC_F18,   KC_F19
+                     RGB_RMOD, RGB_MOD, RGB_SPD, RGB_SPI,
+                     XXXXXXX,  RGB_TOG, RGB_VAD,  RGB_VAI,
+        TO(DEFAULT), QK_BOOT,  XXXXXXX, XXXXXXX,  XXXXXXX,
+        KC_F13,      TO(FN1),  XXXXXXX, XXXXXXX, 
+        KC_F15,      KC_F16,   KC_F17,  KC_F18,   KC_F19
+    ),
+
+    [FN1] = LAYOUT(
+                 XXXXXXX,     XXXXXXX, XXXXXXX,  XXXXXXX,
+                 XXXXXXX,     XXXXXXX, XXXXXXX,  XXXXXXX,
+        TO(FN),  QK_BOOT,     XXXXXXX, XXXXXXX,  XXXXXXX,
+        KC_F13,  TO(DEFAULT), XXXXXXX, XXXXXXX, 
+        KC_F15,  KC_F16,      KC_F17,  KC_F18,   KC_F19
     ),
 };
 
@@ -30,7 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [DEFAULT] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_F13, KC_F14), ENCODER_CCW_CW(KC_F15, KC_F16), ENCODER_CCW_CW(KC_F17, KC_F18), ENCODER_CCW_CW(KC_F19, KC_F20), ENCODER_CCW_CW(KC_F21, KC_F22), ENCODER_CCW_CW(KC_UP, KC_DOWN)  },
     //                                 Encoder 1                              Encoder 2                         Encoder 3                         Encoder 4                     Encoder 5                      Encoder 6                      Encoder 7
-    [FN] =        { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)  }
+    [FN] =        { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)  },
+    //                    Encoder 1                          Encoder 2
+    [FN1] =       { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)  }
     //                    Encoder 1                          Encoder 2
 };
 #endif
@@ -71,6 +82,9 @@ bool oled_task_user(void) {
             break;
         case FN:
             oled_write_P(PSTR("FN\n"), false);
+            break;
+        case FN1:
+            oled_write_P(PSTR("FN1\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
